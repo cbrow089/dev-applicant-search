57ff65adc4c93d81024428d06a5a536765dca216
cbrow089 <cbrow089@yahoo.com>
2024-10-20 15:00:26 -0400

    added the bulk of my working code including components, interfaces, and the proper API calls/endpoints

--- Develop/src/components/Nav.tsx
+++ Develop/src/components/Nav.tsx
@@ -1,8 +1,33 @@
-const Nav = () => {
-  // TODO: Add necessary code to display the navigation bar and link between the pages
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Nav: React.FC = () => {
   return (
-    <div>Nav</div>
-  )
+    <nav className="nav">
+      <ul style={{ display: 'flex', padding: 0, margin: 0 }}>
+        <li className="nav-item">
+          <NavLink 
+            to="/"
+            className={({ isActive }) => 
+              `nav-link ${isActive ? 'active' : ''}`
+            }
+          >
+            Home
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink 
+            to="/SavedCandidates"
+            className={({ isActive }) => 
+              `nav-link ${isActive ? 'active' : ''}`
+            }
+          >
+            Potential Candidates
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
--- Develop/src/interfaces/Candidate.interface.tsx
+++ Develop/src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,14 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+
+interface Candidate {
+    name: string | null;
+    login: string;
+    location: string | null;
+    avatar_url: string;
+    email: string | null;
+    html_url: string;
+    company: string | null;
+    bio: string | null;
+  }
+
+  export default Candidate;
